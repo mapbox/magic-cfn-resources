1af166a9474646956afc675e45a4da50df20b55e
Ryan Clark <ryan.clark.j@gmail.com>
2017-08-22 20:29:30 -0700

    sketch generic resource

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ module.exports.DynamoDbStreamLabel = require('./functions/dynamodb-stream-label'
 module.exports.StackOutputs = require('./functions/stack-outputs').manage;
 module.exports.SpotFleet = require('./functions/spot-fleet').manage;
 
+module.exports.GenericMagicResource = require('./functions/generic');
+
 module.exports.build = require('./lib/build').build;
