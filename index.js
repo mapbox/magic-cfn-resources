01ed42090d3b61fb5d84f935e679aebbef968cf2
Kelly Young <kelly.young@mapbox.com>
2017-09-06 16:54:50 -0700

    exported manage

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports.SnsSubscription = require('./functions/sns-subscription').manage;
 module.exports.DynamoDbStreamLabel = require('./functions/dynamodb-stream-label').manage;
 module.exports.StackOutputs = require('./functions/stack-outputs').manage;
 module.exports.SpotFleet = require('./functions/spot-fleet').manage;
-
+module.exports.S3NotificationTopicConfig = require('./functions/s3-notification-topic-config').manage;
 module.exports.helpers = {
   Response: require('./lib/response'),
   validateEvent: require('./lib/utils').validCloudFormationEvent
