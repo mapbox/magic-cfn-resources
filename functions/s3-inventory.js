eb70fb22aeb995608badb9e9fa270fddd20ad151
Ryan Clark <clark@mapbox.com>
2018-05-14 11:16:56 -0700

    S3Inventory (#26)
    * resource to create S3 inventory
    
    * include inventory in builder
    
    * really forgiving to get things deployed
    
    * IsEnabled boolean
    
    * logs
    
    * keys may not match apis
    
    * debugging time is over, no longer forgiving
    
    * update readme

---
+++ functions/s3-inventory.js
@@ -0,0 +1,104 @@
+var AWS = require('aws-sdk');
+var utils = require('../lib/utils');
+var Response = require('../lib/response');
+
+module.exports = S3Inventory;
+
+function S3Inventory(Bucket, BucketRegion, Id, InventoryConfiguration, OldId) {
+  if (!Bucket)
+    throw new Error('Missing Parameter Bucket');
+  if (!BucketRegion)
+    throw new Error('Missing Parameter BucketRegion');
+  if (!Id)
+    throw new Error('Missing Parameter Id');
+  if (!InventoryConfiguration)
+    throw new Error('Missing Parameter InventoryConfiguration');
+
+  InventoryConfiguration.IsEnabled = InventoryConfiguration.IsEnabled === 'true'
+    ? true : false;
+
+  this.s3 = new AWS.S3({ region: BucketRegion });
+  this.params = { Bucket, Id, InventoryConfiguration };
+
+  if (OldId !== Id) this.oldId = OldId;
+}
+
+S3Inventory.manage = function(event, context, callback) {
+  if (!utils.validCloudFormationEvent(event))
+    return callback(null, 'ERROR: Invalid CloudFormation event');
+
+  var response = new Response(event, context);
+  var requestType = event.RequestType.toLowerCase();
+
+  var s3Inventory;
+  try {
+    s3Inventory = new S3Inventory(
+      event.ResourceProperties.Bucket,
+      event.ResourceProperties.BucketRegion,
+      event.ResourceProperties.Id,
+      event.ResourceProperties.InventoryConfiguration,
+      event.OldResourceProperties ? event.OldResourceProperties.Id : undefined
+    );
+  } catch (err) {
+    return response.send(err);
+  }
+
+  console.log(`${requestType} ${event.ResourceProperties.Bucket}:${event.ResourceProperties.Id}`);
+
+  s3Inventory[requestType](function(err, physicalId) {
+    if (err) return response.send(err);
+    if (physicalId) response.setId(physicalId);
+    response.send();
+  });
+}
+
+S3Inventory.prototype.create = function(callback) {
+  const bucket = this.params.Bucket;
+  const id = this.params.Id;
+  this.s3.putBucketInventoryConfiguration(this.params, function(err) {
+    if (err) return callback(err);
+
+    console.log(`created ${bucket}:${id}`);
+
+    callback(null, id);
+  });
+};
+
+S3Inventory.prototype.update = function(callback) {
+  var old = this.oldId;
+  var s3 = this.s3;
+  var bucket = this.params.Bucket;
+
+  this.create(function(err, id) {
+    if (err) return callback(err);
+    if (!old) return callback(null, id);
+
+    s3.deleteBucketInventoryConfiguration({
+      Bucket: bucket,
+      Id: old
+    }, function (err) {
+      if (err) return callback(err);
+
+      console.log(`deleted ${bucket}:${old}`);
+
+      callback(null, id);
+    });
+
+  })
+};
+
+S3Inventory.prototype.delete = function(callback) {
+  const bucket = this.params.Bucket;
+  const id = this.params.Id;
+
+  this.s3.deleteBucketInventoryConfiguration({
+    Bucket: bucket,
+    Id: id
+  }, function(err) {
+    if (err) return callback(err);
+
+    console.log(`deleted ${bucket}:${id}`);
+
+    callback();
+  });
+};
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports.StackOutputs = require('./functions/stack-outputs').manage;
 module.exports.SpotFleet = require('./functions/spot-fleet').manage;
 module.exports.S3NotificationTopicConfig = require('./functions/s3-notification-topic-config').manage;
 module.exports.DefaultVpc = require('./functions/default-vpc');
+module.exports.S3Inventory = require('./functions/s3-inventory').manage;
 
 module.exports.helpers = {
   Response: require('./lib/response'),
